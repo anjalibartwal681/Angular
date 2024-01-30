82480cd5cda6eb6826f469183730a186f02a78f4
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-30 08:51:21 +0000

    Create ng.ts

---
+++ ng.ts
@@ -0,0 +1,6 @@
+ng stands for Angular
+
+What is ng command in Angular?
+The ng new command creates an Angular workspace folder and generates a new application skeleton.
+A workspace can contain multiple applications and libraries. 
+The initial application created by the ng new command is at the top level of the workspace.
