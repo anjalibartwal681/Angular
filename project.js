cad98a4135c220c8e6f1940a4053bc458965ce49
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-31 07:35:47 +0000

    Create project.js

---
+++ project.js
@@ -0,0 +1,9 @@
+Project development in Angular typically involves a series of steps from setting up the project to deploying it. Below is a high-level overview of the process:
+
+Install Node.js and npm:
+Angular development relies on Node.js and npm. Install Node.js from nodejs.org, and npm (Node Package Manager) will be included.
+
+Install Angular CLI:
+Install the Angular CLI globally using npm. The Angular CLI provides various commands for creating, building, testing, and deploying Angular applications.
+
+npm install -g @angular/cli
