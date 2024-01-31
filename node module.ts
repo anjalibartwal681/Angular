07f4352cb992535c5d021d40bd882831c98d1d44
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-31 05:08:37 +0000

    Create node module.ts

---
+++ node module.ts
@@ -0,0 +1,3 @@
+you can think of a Node module as a collection of functionality bundled together for a specific purpose. 
+A Node module often encapsulates related functions, objects, or classes within a single file, making it a convenient and modular unit of code. 
+These modules can be seen as libraries in the sense that they provide reusable code that can be easily integrated into other projects.
