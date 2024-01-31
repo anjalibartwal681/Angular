c4680f73d20c628821ee894ab86258e9f3936148
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-31 07:34:15 +0000

    Create ngg.ts

---
+++ ngg.ts
@@ -0,0 +1,2 @@
+ng command is shorthand  for various angular cli command line interface  with the abbriviation ng
+cli is  powefull tool that helps  that helps to develop  and build the  angular applicatiion
